113db9897274f796ab0de2d8ec86a826aa19903f
8a-ma <e0as0k98x001b@gmail.com>
2023-11-22 17:20:41 -0300

    Añadido de cartelera

--- src/components/Carta.js
+++ src/components/Carta.js
@@ -1,10 +1,8 @@
 function Carta(props) {
     return (
-        <div className="card">
-            <img src={props.src} className="card-img-top w-50" alt='...' />
-            <div className="card-body">
-                <p className="card-text"> {props.name} </p>
-            </div>
+        <div key={props.id} className="col-5">
+            <img src={props.src} className="img-fluid w-75" alt='...' />
+            <p className="text-center"> {props.name} </p>
         </div>
     );
 }
--- src/components/Cartelera.js
+++ src/components/Cartelera.js
@@ -1,50 +1,46 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Carta from "./Carta";
 
 function Cartelera(){
+    const API_URL = 'https://api.themoviedb.org/3';
+    const API_KEY = 'ef2c3ae3a5d5a1de391beafbfc9d5b06';
+    const IMAGE_PATH = 'https://image.tmdb.org/t/p/original';
+    const URL_IMAGE = 'https://image.tmdb.org/t/p/original';
+
+    //Variables de estado
+    const [movies, setMovies] = useState([]);
+    const [searchKey, setSearchKey] = useState("");
+    const [trailer, setTrailer] = useState(null);
+    const [movie, setMovie] = useState({title: "Loading Movies"}); 
+    const [playing, setPlaying] = useState(false);
+
+    //Funcion para hacer peticion get a la api de tmdb
+    const fetchMovies = async(searchKey) =>{
+        const type = searchKey ? "search" : "discover"
+        const {data: { results } ,} = await axios.get(`${API_URL}/${type}/movie`, {
+            params: {
+                api_key: API_KEY,
+                query: searchKey,
+            },  
+        });
+
+        setMovies(results);
+        setMovie(results[0]);
+    }
+
+    useEffect(()=>{
+        fetchMovies();
+    }, [])
+
     return (
         <div className="container text-center p-1">
-            <div className="row align-item-center grid p-1">
-                <div className="col">
-                    <Carta name="1" src="https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" />
-                </div>
-                <div className="col">
-                    <Carta name="2" />
-                </div>
-            </div>
-            <div className="row align-item-center grid p-1">
-                <div className="col">
-                    <Carta name="3" />
-                </div>
-                <div className="col">
-                    <Carta name="4" />
-                </div>
-            </div>
-            <div className="row align-item-center grid p-1">
-                <div className="col">
-                    <Carta name="5" />
-                </div>
-                <div className="col">
-                    <Carta name="6" />
-                </div>
-            </div>
-            <div className="row align-item-center grid p-1">
-                <div className="col">
-                    <Carta name="7" />
-                </div>
-                <div className="col">
-                    <Carta name="8" />
-                </div>
-            </div>
-            <div className="row align-item-center grid p-1">
-                <div className="col">
-                    <Carta name="9" />
-                </div>
-                <div className="col">
-                    <Carta name="10" />
-                </div>
+            <div className="row mx-auto pt-1">
+                {movies.map((movie)=>(
+                    <Carta id={movie.id} name={movie.title} src={`${URL_IMAGE + movie.poster_path}`} />
+                ))}
             </div>
         </div>
-        
     );
 }
 
