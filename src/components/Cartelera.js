47a6d832dfee41e1e4360febacc9c90d61b873e5
8a-ma <e0as0k98x001b@gmail.com>
2023-11-22 17:35:55 -0300

    Modificacion en cartelera

--- src/components/Cartelera.js
+++ src/components/Cartelera.js
@@ -1,26 +1,21 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Carta from "./Carta";
+import * as API from '../const.js';
 
 function Cartelera(){
-    const API_URL = 'https://api.themoviedb.org/3';
-    const API_KEY = 'ef2c3ae3a5d5a1de391beafbfc9d5b06';
-    const IMAGE_PATH = 'https://image.tmdb.org/t/p/original';
-    const URL_IMAGE = 'https://image.tmdb.org/t/p/original';
 
     //Variables de estado
     const [movies, setMovies] = useState([]);
     const [searchKey, setSearchKey] = useState("");
-    const [trailer, setTrailer] = useState(null);
     const [movie, setMovie] = useState({title: "Loading Movies"}); 
-    const [playing, setPlaying] = useState(false);
 
     //Funcion para hacer peticion get a la api de tmdb
     const fetchMovies = async(searchKey) =>{
         const type = searchKey ? "search" : "discover"
-        const {data: { results } ,} = await axios.get(`${API_URL}/${type}/movie`, {
+        const {data: { results } ,} = await axios.get(`${API.API_URL}/${type}/movie`, {
             params: {
-                api_key: API_KEY,
+                api_key: API.API_KEY,
                 query: searchKey,
             },  
         });
@@ -37,7 +32,7 @@ function Cartelera(){
         <div className="container text-center p-1">
             <div className="row mx-auto pt-1">
                 {movies.map((movie)=>(
-                    <Carta id={movie.id} name={movie.title} src={`${URL_IMAGE + movie.poster_path}`} />
+                    <Carta id={movie.id} name={movie.title} src={`${API.URL_IMAGE + movie.poster_path}`} />
                 ))}
             </div>
         </div>
---
+++ src/const.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://api.themoviedb.org/3';
+export const API_KEY = 'ef2c3ae3a5d5a1de391beafbfc9d5b06';
+export const IMAGE_PATH = 'https://image.tmdb.org/t/p/original';
+export const URL_IMAGE = 'https://image.tmdb.org/t/p/original';
