1f917e7972a2471a9d69b98d93bfe73b4cf4db51
8a-ma <e0as0k98x001b@gmail.com>
2023-11-22 19:26:59 -0300

    Buscador funcional

--- src/Pages/Inicio.js
+++ src/Pages/Inicio.js
@@ -1,14 +1,55 @@
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
-import Cartelera from '../components/Cartelera';
+import Carta from '../components/Carta';
 import Footer from '../components/Footer';
 
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import * as API from '../const.js';
+
 function Inicio(){
+
+    //Variables de estado
+    const [movies, setMovies] = useState([]);
+    const [searchKey, setSearchKey] = useState("");
+    const [movie, setMovie] = useState({title: "Loading Movies"}); 
+
+    //Funcion para hacer peticion get a la api de tmdb
+    const fetchMovies = async(searchKey) =>{
+        const type = searchKey ? "search" : "discover"
+        const {data: { results } ,} = await axios.get(`${API.API_URL}/${type}/movie`, {
+            params: {
+                api_key: API.API_KEY,
+                query: searchKey,
+            },  
+        });
+
+        setMovies(results);
+        setMovie(results[0]);
+    }
+
+    const searchMovies = (e)=>{
+        e.preventDefault();
+        fetchMovies(searchKey)
+    }
+
+    useEffect(()=>{
+        fetchMovies();
+    }, [])
+
+
+
     return (
         <div>
             <Header />
-            <Navbar />
-            <Cartelera />
+            <Navbar searchMovies={searchMovies} setSearchKey={setSearchKey} />
+            <div className="container text-center p-1">
+                <div className="row mx-auto pt-1">
+                    {movies.map((movie)=>(
+                        <Carta id={movie.id} name={movie.title} src={`${API.URL_IMAGE + movie.poster_path}`} />
+                    ))}
+                </div>
+            </div>
             <Footer />
         </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Navbar(){
+function Navbar(props){
     return (
         <div>
             <nav className='navbar navbar-expand-lg bg-custom-nav'>
@@ -12,7 +12,11 @@ function Navbar(){
                         <li className='nav-item'><Link to="/contacto" className='nav-link link-light'>Contacto</Link></li>
                         <li className='nav-item'><Link to="/carrito" className='nav-link link-light'>Carrito</Link></li>
                     </ul>
-                    <p className='nav-item d-flex text-light'>Barra de Busqueda</p>
+                    
+                    <form className='nav-item d-flex text-light p-1' onSubmit={props.searchMovies}>
+                        <input type="text" placeholder='Buscar' onChange={(e)=> props.setSearchKey(e.target.value)} className='mx-1'/>
+                        <button className='btn btn-primary'>Buscar</button>
+                    </form>
                 </div>
             </nav>
         </div>
