c50dd8fd9e9392dd6d0a5af47a28f1c54bfba40b
8a-ma <e0as0k98x001b@gmail.com>
2023-11-22 22:07:32 -0300

    Informacion por pelicula 2 (por terminar)

--- src/Pages/Pelicula.js
+++ src/Pages/Pelicula.js
@@ -18,16 +18,37 @@ function Pelicula() {
                 <div className='my-2 row'>
                     <div className='col text-center'>
                         <p className='h1 text-white fw-light'>{movie.title}</p>
-                        <img src={`${API.URL_IMAGE + movie.poster_path}`} className='w-25' />
+                        <img src={`${API.URL_IMAGE + movie.poster_path}`} className='w-50' />
                     </div>
-                    <div className='col'>
-                        <div>Sinopsis</div>
-                        <div>Ranking IMDB</div>
-                        <div>Elenco</div>
-                        <div>Duración</div>
-                        <div>Género</div>
-                        <div>Horario</div>
-                        <div>Trailer</div>
+                    <div className='col text-white fw-light'>
+                        <div>
+                            <p className="h4">Sinopsis</p>
+                            <p>{movie.overview}</p>
+                        </div>
+                        <div>
+                            <p className="h4">Ranking IMDB</p>
+                            <p>{movie.vote_average}</p>
+                        </div>
+                        <div>
+                            <p className='h4'>Elenco</p>
+                            <p>POR AGREGAR</p>
+                        </div>
+                        <div>
+                            <p className='h4'>Duración</p>
+                            <p>POR AGREGAR</p>
+                        </div>
+                        <div>
+                            <p className='h4'>Género/s</p>
+                            <p>POR AGREGAR</p>
+                        </div>
+                        <div>
+                            <p className='h4'>Horarios</p>
+                            <p>13:00, 15:30, 19:20, 22:00</p>
+                        </div>
+                        <div>
+                            <p className='h4'>Trailer</p>
+                            <p>POR AGREGAR</p>
+                        </div>
                     </div>
                     
 
